04d547f13f9f1d3efc2125b24af9892ed5a744e7
KevinYuan <destinydoor@163.com>
2019-12-16 01:24:34 +0800

    Signed-off-by: KevinYuan <destinydoor@163.com>

---
+++ DiverseUI/app/src/main/java/com/kevin/diverseui/XCRoundRectImageView.java
@@ -0,0 +1,107 @@
+package com.kevin.diverseui;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+/**
+ * 自定义的圆角矩形ImageView，可以直接当组件在布局中使用。
+ * @author caizhiming
+ *
+ */
+public class XCRoundRectImageView extends AppCompatImageView {
+
+    private Paint paint;
+
+    public XCRoundRectImageView(Context context) {
+        this(context,null);
+    }
+
+    public XCRoundRectImageView(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public XCRoundRectImageView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        paint  = new Paint();
+    }
+
+    /**
+     * 绘制圆角矩形图片
+     * @author caizhiming
+     */
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Drawable drawable = getDrawable();
+        if (null != drawable) {
+            Bitmap b = getRoundBitmap(drawable2Bitmap(drawable), dp2px(getContext(),7));
+            final Rect rectSrc = new Rect(0, 0, b.getWidth(), b.getHeight());
+            final Rect rectDest = new Rect(0,0,getWidth(),getHeight());
+            paint.reset();
+            canvas.drawBitmap(b, rectSrc, rectDest, paint);
+        } else {
+            super.onDraw(canvas);
+        }
+    }
+
+    /**
+     * drawable转换成bitmap
+     */
+    private Bitmap drawable2Bitmap(Drawable drawable) {
+        if (drawable == null) {
+            return null;
+        }
+        Bitmap bitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        //根据传递的scaletype获取matrix对象，设置给bitmap
+        Matrix matrix = getImageMatrix();
+        if (matrix != null) {
+            canvas.concat(matrix);
+        }
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+
+    /**
+     * 获取圆角矩形图片方法
+     * @param bitmap
+     * @param roundPx,一般设置成14
+     * @return Bitmap
+     * @author caizhiming
+     */
+    private Bitmap getRoundBitmap(Bitmap bitmap, int roundPx) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_4444);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+        final RectF rectF = new RectF(rect);
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+
+    public static int dp2px(Context context, float dp) {
+        float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+}
